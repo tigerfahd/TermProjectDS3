4ff05c4bfc313fae50003d09cc4b23dabf4a4860
Alyssa Byer <byer0064@algonquinlive.com>
2019-04-05 10:57:21 -0400

    added property cards and changed locations and sizes

--- server/static/js/create-card-component.js
+++ server/static/js/create-card-component.js
@@ -21,18 +21,19 @@ AFRAME.registerComponent('create-card-component', {
         var newnum = parseInt(strOut);
         switch (newnum) {
             case 1:
-                cardEl.setAttribute('obj-model', {obj:'/assets/models/card.obj'});
-                cardEl.setAttribute('material', {src:'/assets/textures/propertyCard_tex.jpg'});
+                cardEl.setAttribute('obj-model', {obj:'/assets/models/PropertyCard_Japan.obj'});
+                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_AtlanticOcean.png'});
                 cardEl.setAttribute('position', {x:15, y:6, z:58});
               break;
             case 2:
                 cardEl.setAttribute('obj-model', {obj:'/assets/models/card.obj'});
                 cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_Egypt.png'});
                 cardEl.setAttribute('position', {x:6, y:-1, z:32});
+                cardEl.setAttribute('scale', {x:0.04, y:0.04, z:0.04});
               break;
             case 3:
-                cardEl.setAttribute('obj-model', {obj:'/assets/models/card.obj'});
-                cardEl.setAttribute('material', {src:'/assets/textures/propertyCard_tex.jpg'});
+                cardEl.setAttribute('obj-model', {obj:'/assets/PropertyCard_Japan.obj'});
+                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_IndianOcean.png'});
                 cardEl.setAttribute('position', {x:15, y:6, z:58});
               break;
             case 4:
@@ -42,33 +43,34 @@ AFRAME.registerComponent('create-card-component', {
               break;
             case 5:
                 cardEl.setAttribute('obj-model', {obj:'/assets/models/PropertyCard_Japan.obj'});
-                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_Japan.png'});
+                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_PacificOcean.png'});
                 cardEl.setAttribute('position', {x:15, y:2, z:48});
               break;
             case 6:
                 cardEl.setAttribute('obj-model', {obj:'/assets/models/PropertyCard_Japan.obj'});
-                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_Japan.png'});
-                cardEl.setAttribute('position', {x:15, y:2, z:48});
+                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_Greece.png'});
+                cardEl.setAttribute('position', {x:10, y:2, z:48});
               break;
             case 7:
                 cardEl.setAttribute('obj-model', {obj:'/assets/models/PropertyCard_Japan.obj'});
-                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_Japan.png'});
+                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_ArcticOcean.png'});
                 cardEl.setAttribute('position', {x:15, y:2, z:48});
               break;
             case 8:
                 cardEl.setAttribute('obj-model', {obj:'/assets/models/PropertyCard_Japan.obj'});
-                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_Japan.png'});
-                cardEl.setAttribute('position', {x:15, y:2, z:48});
+                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_India.png'});
+                cardEl.setAttribute('position', {x:-94, y:18, z:181.575});
+                cardEl.setAttribute('scale', {x:0.124, y:0.124, z:0.124});
               break;
             case 9:
                 cardEl.setAttribute('obj-model', {obj:'/assets/models/PropertyCard_Japan.obj'});
-                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_Japan.png'});
-                cardEl.setAttribute('position', {x:15, y:2, z:48});
+                cardEl.setAttribute('material', {src:'/assets/textures/PropertyCard_Peru.png'});
+                cardEl.setAttribute('position', {x:4, y:1.2, z:288});
+                cardEl.setAttribute('scale', {x:0.07, y:0.07, z:0.07});
               break;
           }
         
         cardEl.setAttribute('remove-component', {}); 
-        cardEl.setAttribute('scale', {x:0.04, y:0.04, z:0.04});
         let scene = document.querySelector('a-scene');
         scene.appendChild(cardEl);
     }
